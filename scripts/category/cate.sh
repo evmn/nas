1c8c60b054a6a7a71b20363b98ba38aeaa8afe25
evmn <38780274+evmn@users.noreply.github.com>
2020-10-13 19:40:55 +0800

    deal with other file in background thread, create a function to deal with files

--- scripts/category/cate.sh
+++ scripts/category/cate.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # 在Centos 7及Ubuntu 20.04测试，可以正确处理包含空格的文件
-for folder in */;
-do
-        echo "$folder"
-        cd "$folder"
+move_file(){
         for i in {01..10};
         do
                 # 遍历子目录查找类似s01e02的文件，将来设置为不处理文件夹
@@ -13,7 +10,8 @@ do
                         then
                                 mkdir "Season $i"
                         fi
-                        #将来让此命令在后台执行或许可以加快速度
+                        # 不能在后台运行过多的命令，网络缓存会让下面的Specials先执行
+			# 除非先用正则表达式匹配并处理不包含[sS]${i}[eE]的文件
                         mv *[sS]${i}[eE]* "Season $i/"
                 fi
         done
@@ -26,9 +24,18 @@ do
                         then
                                 mkdir Specials
                         fi
-                        mv "$other" Specials/
+                        mv "$other" Specials/ &
                         #将其它文件放到Specials目录中
                 fi
         done
+}
+
+
+for folder in */;
+do
+        echo "$folder"
+        cd "$folder"
+	move_file 
+	sleep 10
         cd ..
 done
